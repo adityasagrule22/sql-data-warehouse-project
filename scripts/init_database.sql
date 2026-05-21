36a030e48fa72724dbcca26a146b4b2c3a91289a
Aditya Sagrule <adityasagrule569@gmail.com>
2026-05-21 12:11:15 +0100

    Create Datawarehouse database and schemas
    This script creates a new database named 'Datawarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It also includes logic to drop the existing database if it already exists.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,31 @@
+/*
+===========================================
+Create Database and Schemas
+===========================================
+Script Purpose:
+    This script creates a new database named 'Datawarehouse' after checking if it already exists.
+    If the database exsts, it is dropped and recreated. Additionally, the scriot sets up three schemas within the databse: 'bronze', 'silver', 'gold'.
+/*
+USE master;
+GO
 
+-- Drop and recreate the 'Datawarehouse' database
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'Datawarehouse')
+BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABSE Datawarehouse;
+END;
+GO
+
+-- Create the 'Datawarehouse' database
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
